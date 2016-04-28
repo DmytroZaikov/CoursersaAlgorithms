180475a06f5556e31ae8182fd30ccc270f683926
DmytroZaikov <dmytro.zaikov@gmail.com>
2016-04-28 21:58:57 +0200

    Insertion sort implemented

--- Part1/src/elementarysorts/Client.java
+++ Part1/src/elementarysorts/Client.java
@@ -3,30 +3,57 @@ package elementarysorts;
 import java.util.Random;
 
 public class Client {
-    private static int ARRAY_LENGTH = 10;
+    private static int ARRAY_LENGTH = 10000;
 
     private static int randInt(int min, int max) {
         Random rand = new Random();
         return rand.nextInt((max - min) + 1) + min;
     }
 
-    public static void main(String[] args){
-        Integer[] arrayToBeSorted = new Integer[ARRAY_LENGTH];
-
+    private static Integer[] arrayOfIntInit(){
+        Integer[] a = new Integer[ARRAY_LENGTH];
         for (int i = 0; i < ARRAY_LENGTH; i++) {
-            arrayToBeSorted[i] = randInt(1, 10);
+            a[i] = randInt(1, 10);
         }
+        return a;
+    }
 
+    public static void main(String[] args){
+
+        Integer[] a1 = arrayOfIntInit();
+        Integer[] a2 = arrayOfIntInit();
+
+        System.out.println("Selection Sort:");
         System.out.println("Before sorting...");
-        for (int num: arrayToBeSorted) {
+        for (int num: a1) {
             System.out.print(num + " ");
         }
+        long startTime = System.currentTimeMillis();
+        SelectionSorts.sort(a1);
+        long stopTime = System.currentTimeMillis();
 
-        SelectionSorts.sort(arrayToBeSorted);
+        long elapsedTime = stopTime- startTime;
 
         System.out.println("\nAfter sorting...");
-        for (int num: arrayToBeSorted) {
+        for (int num: a1) {
             System.out.print(num + " ");
         }
+        System.out.println("\nElapsed time: " + elapsedTime);
+
+        System.out.println("Insertion Sort:");
+        System.out.println("Before sorting...");
+        for (int num: a2) {
+            System.out.print(num + " ");
+        }
+        startTime = System.currentTimeMillis();
+        InsertionSort.sort(a2);
+        stopTime = System.currentTimeMillis();
+        elapsedTime = stopTime- startTime;
+
+        System.out.println("\nAfter sorting...");
+        for (int num: a2) {
+            System.out.print(num + " ");
+        }
+        System.out.println("\nElapsed time: " + elapsedTime);
     }
 }
--- Part1/src/elementarysorts/SelectionSorts.java
+++ Part1/src/elementarysorts/SelectionSorts.java
@@ -7,13 +7,13 @@ public class SelectionSorts {
 
     public static void sort(Comparable[] a){
 
-        int arraySize = a.length;
+        int N = a.length;
 
-        for (int i = 0; i < arraySize; i++) {
+        for (int i = 0; i < N; i++) {
 
             int min = i;
 
-            for (int j = i+1; j < arraySize; j++) {
+            for (int j = i+1; j < N; j++) {
                 if(isLess(a[j], a[min])){
                     min = j;
                 }
