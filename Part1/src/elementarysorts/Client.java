7dc22b762fe44ed61612356feb53d55cf2cd1f47
DmytroZaikov <dmytro.zaikov@gmail.com>
2016-04-28 22:20:24 +0200

    Client modified considering newly created ArraysHelper and Timerz classes

--- Part1/src/elementarysorts/Client.java
+++ Part1/src/elementarysorts/Client.java
@@ -1,59 +1,38 @@
 package elementarysorts;
 
-import java.util.Random;
+import commonhelpers.ArraysHelper;
+import commonhelpers.Timerz;
+
 
 public class Client {
-    private static int ARRAY_LENGTH = 10000;
-
-    private static int randInt(int min, int max) {
-        Random rand = new Random();
-        return rand.nextInt((max - min) + 1) + min;
-    }
-
-    private static Integer[] arrayOfIntInit(){
-        Integer[] a = new Integer[ARRAY_LENGTH];
-        for (int i = 0; i < ARRAY_LENGTH; i++) {
-            a[i] = randInt(1, 10);
-        }
-        return a;
-    }
 
     public static void main(String[] args){
-
-        Integer[] a1 = arrayOfIntInit();
-        Integer[] a2 = arrayOfIntInit();
+        Timerz t = new Timerz();
+        Integer[] a1 = ArraysHelper.arrayOfIntInit();
+        Integer[] a2 = ArraysHelper.arrayOfIntInit();
 
         System.out.println("Selection Sort:");
-        System.out.println("Before sorting...");
-        for (int num: a1) {
-            System.out.print(num + " ");
-        }
-        long startTime = System.currentTimeMillis();
+        System.out.println("Before sorting:");
+        ArraysHelper.arrayPrettyPrint(a1);
+
+        t.start();
         SelectionSorts.sort(a1);
-        long stopTime = System.currentTimeMillis();
+        t.stop();
 
-        long elapsedTime = stopTime- startTime;
-
-        System.out.println("\nAfter sorting...");
-        for (int num: a1) {
-            System.out.print(num + " ");
-        }
-        System.out.println("\nElapsed time: " + elapsedTime);
+        System.out.println("After sorting:");
+        ArraysHelper.arrayPrettyPrint(a1);
+        System.out.println("Elapsed time: " + t.getElapsedTime());
 
         System.out.println("Insertion Sort:");
-        System.out.println("Before sorting...");
-        for (int num: a2) {
-            System.out.print(num + " ");
-        }
-        startTime = System.currentTimeMillis();
-        InsertionSort.sort(a2);
-        stopTime = System.currentTimeMillis();
-        elapsedTime = stopTime- startTime;
+        System.out.println("Before sorting:");
+        ArraysHelper.arrayPrettyPrint(a2);
 
-        System.out.println("\nAfter sorting...");
-        for (int num: a2) {
-            System.out.print(num + " ");
-        }
-        System.out.println("\nElapsed time: " + elapsedTime);
+        t.start();
+        InsertionSort.sort(a2);
+        t.stop();
+
+        System.out.println("After sorting:");
+        ArraysHelper.arrayPrettyPrint(a2);
+        System.out.println("Elapsed time: " + t.getElapsedTime());
     }
 }
